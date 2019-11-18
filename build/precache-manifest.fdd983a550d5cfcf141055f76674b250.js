0a31a764664864fc2fbb3d53741a4f0c3e3b7130
Dora Chan <dora.lchan@gmail.com>
2019-11-18 09:27:09 -0800

    pushing build

---
+++ build/precache-manifest.fdd983a550d5cfcf141055f76674b250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a066c2d1c5629a2f38b5ff3e0e15231",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cde7c874d7f429337ecc",
+    "url": "/static/css/main.3cce5636.chunk.css"
+  },
+  {
+    "revision": "8a3b87e6c43e2b18b164",
+    "url": "/static/js/2.00a07f20.chunk.js"
+  },
+  {
+    "revision": "cde7c874d7f429337ecc",
+    "url": "/static/js/main.06f2b331.chunk.js"
+  },
+  {
+    "revision": "4c98106a8903d35814cb",
+    "url": "/static/js/runtime-main.98db68b5.js"
+  }
+]);
