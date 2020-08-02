acdc188ad26ea194ae46923bc66964541859d508
Dora Chan <dora.lchan@gmail.com>
2020-08-02 08:32:57 -0700

    clear out

--- src/preview.js
+++ src/preview.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Logo from './components/logo';
 
 import styled from '@emotion/styled';
 
 function Preview() {
   return (
     <Landing>
-      <Logo fullLogo={false} />
-      <Message>      
-        <h1>Sentry Design System</h1>
-        <Subtext>Coming soon so hold onto your butts</Subtext>
-      </Message>
+      TBD
     </Landing>
   );
 }
@@ -21,20 +16,8 @@ const Landing = styled('div')`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  color: #ffffff;
-  background-color: #120F15;
+  color: #120F15;
 `;
 
-const Message = styled('div')`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 8px 0 24px 0;
-`;
-
-const Subtext = styled('p')`
-  margin: 8px;
-  color: #4A3E56;
-`;
 
 export default Preview;
