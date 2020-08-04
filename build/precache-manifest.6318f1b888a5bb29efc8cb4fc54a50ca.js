c82771b22b49930d4e36e0c5d65a89b18de2256a
Dora Chan <dora.lchan@gmail.com>
2020-08-04 10:40:01 -0700

    build

---
+++ build/precache-manifest.6318f1b888a5bb29efc8cb4fc54a50ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2343101c1fd43c7670e27190fb4196",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bd7974c1a022b57f942",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ff9a827279075e8b0916",
+    "url": "/static/js/2.f47985b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f47985b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bd7974c1a022b57f942",
+    "url": "/static/js/main.681b1e9c.chunk.js"
+  },
+  {
+    "revision": "416be146ddb5e02c6522",
+    "url": "/static/js/runtime-main.4a55df43.js"
+  }
+]);
